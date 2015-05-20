9bf8d12df713e2d06cbbf21958e2af739d867775
Rahul Bansal <rahul.bansal@rtcamp.com>
2015-05-20 16:53:58 +0530

    Fixes active chapter's children visibility

--- book/plugin.js
+++ book/plugin.js
@@ -2,6 +2,7 @@ require(["gitbook"], function(gitbook) {
     gitbook.events.bind("page.change", function() {
         $('ul.summary li li').hide();
         // $('ul.summary li').find('li.active').parent().children().show();
-        $('ul.summary li li.active').parents().children().show();        
+        $('ul.summary li li.active').parents().children().show();
+        $('ul.summary li.active li').show();        
     });
 });
